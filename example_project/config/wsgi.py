e085dbedcff721cf7e59b592ad2ca329e4d5ee04
Matti Lamppu <lamppu.matti.akseli@gmail.com>
2024-09-05 16:58:24 +0300

    Move project to root level

--- example_project/config/wsgi.py
+++ example_project/config/wsgi.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example_project.config.settings")
 
 from django.core.wsgi import get_wsgi_application
 
